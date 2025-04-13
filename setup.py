5b1f33df303a2d74b808fb5751608efebd0b5443
Irving Hernández <irving.hernandez.ag@gmail.com>
2025-04-12 20:23:21 -0600

    First Release of Package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="learningReleases",                                           # Nombre del paquete
+    version="0.1.0",                                                   # Versión inicial
+    packages=find_packages(),                                          # Paquetes a incluir
+    description="Un paquete pip simple de saludo",                     # Breve descripción
+    author="Irving Hernández",                                         # Tu nombre
+    author_email="irving.hernandez@gmail.com",                         # Tu correo electrónico
+    url="https://github.com/Irving-Main-Ophelia/learningReleases",     # URL del proyecto
+)
